2f599d1151393f945c1d94e26d3dfd5d16c8f4af
Madhuvanth123 <24502721cse1@gmail.com>
2025-08-11 11:41:45 +0530

    Update Python.py

--- Python.py
+++ Python.py
@@ -1,5 +1,6 @@
 name = input("What is your name? ")
-age = int(input("How old are you? "))
+age = int(input("MADHUVANTH"))
 future_age = age + 10
 print(f"Hello, {name}! You will be {future_age}years old in 10 years.")
+
 
